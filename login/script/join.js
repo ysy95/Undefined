b36e8ce3f066a83d20a89b5b7170f2f6bcd001fb
LDH9276 <leedh9276@naver.com>
2023-03-13 12:25:30 +0900

    동헌 : 3월 13일 로그인 스크립트 추가 및 수정

---
+++ login/script/join.js
@@ -0,0 +1,48 @@
+const c01 = document.querySelector("#c01");
+const c02 = document.querySelector("#lc01");
+const modal = document.querySelector(".modal");
+
+
+c01.addEventListener("click", (e) => {
+  e.preventDefault();
+  modal.style = "display: flex";
+});
+c02.addEventListener("click", (e) => {
+  e.preventDefault();
+  modal.style = "display: flex";
+});
+
+function modal_chk() {
+  modal.style = "display: none";
+  c01.checked = true;
+}
+
+const submitBtn = document.querySelector("input[type=submit]");
+const email = document.querySelector("#Email");
+const id = document.querySelector("#ID");
+const pw = document.querySelector("#PW");
+const pw2 = document.querySelector("#PWChk");
+const errorBox = document.querySelector(".error");
+
+submitBtn.addEventListener("click", (e) => {
+  if (email.value == "") {
+    e.preventDefault();
+    email.focus();
+    errorBox.innerText = "이메일을 입력해주세요.";
+  } else if (id.value == "") {
+    e.preventDefault();
+    id.focus();
+    errorBox.innerText = "아이디를 입력해주세요.";
+  } else if (pw.value == "") {
+    e.preventDefault();
+    pw.focus();
+    errorBox.innerText = "비밀번호를 입력해주세요.";
+  } else if (pw.value !== pw2.value) {
+    e.preventDefault();
+    pw.focus();
+    errorBox.innerText = "비밀번호가 일치하지 않습니다.";
+  } else if (c01.checked == false) {
+    e.preventDefault();
+    errorBox.innerText = "개인정보 수집 및 이용에 동의해주세요.";
+  }
+});
