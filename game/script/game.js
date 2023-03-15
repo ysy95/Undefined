8ddd5224a0b13545444237481070bf2496d0dba2
xanadu13 <xanadu13>
2023-03-15 12:41:52 +0900

    게임페이지 최종완료

--- game/script/game.js
+++ game/script/game.js
@@ -16,7 +16,7 @@ $(function () {
     $('.newgame-banner li').addClass('display-none').removeClass('newgame-banner-on');
     $('.newgame-banner-l video').get(game_index).currentTime = 0;
     $('.newgame-banner li').eq(game_index).removeClass('display-none').addClass('newgame-banner-on');
-    $('#newgame').css('background-image', `url(../img/gamebanner_bg_0${game_index}.jpg)`);
+    $('#newgame').css('background-image', `url(./img/gamebanner_bg_0${game_index}.jpg)`);
     $('.newgame-banner-list > li').find('div').removeClass('gage-on');
     $('.newgame-banner-list > li').eq(game_index).find('div').addClass('gage-on');
     $('.newgame-banner-list > li').eq(game_index).find('span').css('width', '0%');
@@ -32,7 +32,7 @@ $(function () {
     $('.newgame-banner li').addClass('display-none').removeClass('newgame-banner-on');
     $('.newgame-banner-l video').get(game_index).currentTime = 0;
     $('.newgame-banner li').eq(game_index).removeClass('display-none').addClass('newgame-banner-on');
-    $('#newgame').css('background-image', `url(../img/gamebanner_bg_0${game_index}.jpg)`);
+    $('#newgame').css('background-image', `url(./img/gamebanner_bg_0${game_index}.jpg)`);
     $('.newgame-banner-list > li').find('div').removeClass('gage-on');
     $('.newgame-banner-list > li').eq(game_index).find('div').addClass('gage-on');
     $('.newgame-banner-list > li').eq(game_index).find('span').css('width', '0%');
@@ -174,52 +174,120 @@ $(function () {
   });
 
 
-
-
-});
-
-
-
-$(function(){
-  //////////// json 게임리스트 삽입 스크립트 ////////////
-
-  $('.game-btn').click(function () {
-    $(this).hide(); // 더보기버튼 숨기기
-
-    $.ajax({
-      url: './game.json',
-      type: 'post',
-      dataType: 'json',
-      success: function (result) {
-        let t = '<ul class="gamelist-list">';
-        $.each(result.game, function(i,e) {
-          t += `
-            <li>
-              <div class="gamelist-gameinfo">
-                <div class="gamelist-img ${e.img_class}"></div>
-                <figure>
-                  <img src="./img/${e.icon_path}" alt="${e.title} 로고">
-                  <figcaption>
-                    <p class="gamelist-title">${e.title}</p>
-                    <p class="gamelist-type">${e.type}</p>
-                  </figcaption>
-                </figure>
-              </div>
-              <div class="gamelist-gameinfo-hover display-none">
-                <a href="#" target="_blank" title="${e.title} 게임설명 바로가기"><img src="./img/${e.logo_path}" alt="${e.title} 로고"></a>
-                <a href="#" target="_blank" title="${e.title} 공식사이트 바로가기">공식사이트<i class="fas fa-angle-right"></i></a>
-                <a href="#" target="_blank" title="${e.title} 구글플레이 바로가기"><img src="./img/btn_playstore.png" alt="구글플레이 바로가기"></a>
-                <a href="#" target="_blank" title="${e.title} 앱스토어 바로가기"><img src="./img/btn_appstore.png" alt="앱스토어 바로가기"></a>
-              </div>
-              <div class="gamelist-margin"></div>
-            </li>
-          `;
-        });
-        t += "</ul>";
-        //데이터를 t변수에 담아서 list박스에 내용을 출력한다.
-        $('.gamelist-list-add').html(t);
-      }
-    });
-    return false;
+  // 3. PC에서만 더보기 버튼 나오게 하기
+  // 단, 다시 PC 눌렀을때는 더보기 버튼이 나오게 하기
+  $('.gamelist-tab > li:first-child').click(function () {
+    $('.game-btn').show();
+    $('.gamelist-list-add').hide();
   });
+
+  $('.gamelist-tab > li:nth-child(2)').click(function () {
+    $('.gamelist-list-add').hide();
+    $('.game-btn').hide();
+  });
+
+  $('.gamelist-tab > li:last-child').click(function () {
+    $('.gamelist-list-add').hide();
+    $('.game-btn').hide();
+  });
+
+
+  // 4. 더보기 버튼 클릭시 추가
+  $('.game-btn').click(function(){
+    $('.gamelist-list-add').html(content);
+    $('.gamelist-list-add').show();
+    $(this).hide();
+
+    
+  // 5. 더보기된 컨텐츠 마우스오버
+    $('.gamelist-list2 li').hover(function () {
+      $(this).find('.gamelist-gameinfo-hover').addClass('on')
+    }, function () {
+      $(this).find('.gamelist-gameinfo-hover').removeClass('on')
+    });
+  
+  });
+
+  let content =
+  `
+  <ul class="gamelist-list2">
+    <li>
+      <div class="gamelist-gameinfo">
+        <div class="gamelist-img img09"></div>
+        <figure>
+          <img src="./img/gamelist09.png" alt="쿵야 캐치마인드 로고">
+          <figcaption>
+            <p class="gamelist-title">쿵야 캐치마인드</p>
+            <p class="gamelist-type">캐주얼</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="gamelist-gameinfo-hover">
+        <a href="#" target="_blank" title="쿵야 캐치마인드 게임설명 바로가기"><img src="./img/hover_gamelist09.png" alt="쿵야 캐치마인드 로고"></a>
+        <a href="#" target="_blank" title="쿵야 캐치마인드 공식사이트 바로가기">공식사이트<i class="fas fa-angle-right"></i></a>
+        <a href="#" target="_blank" title="쿵야 캐치마인드 구글플레이 바로가기"><img src="./img/btn_playstore.png" alt="구글플레이 바로가기"></a>
+        <a href="#" target="_blank" title="쿵야 캐치마인드 앱스토어 바로가기"><img src="./img/btn_appstore.png" alt="앱스토어 바로가기"></a>
+      </div>
+    </li>
+
+    <li>
+      <div class="gamelist-gameinfo">
+        <div class="gamelist-img img10"></div>
+        <figure>
+          <img src="./img/gamelist10.png" alt="마구마구 2023 로고">
+          <figcaption>
+            <p class="gamelist-title">마구마구 2023</p>
+            <p class="gamelist-type">스포츠</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="gamelist-gameinfo-hover">
+        <a href="#" target="_blank" title="마구마구 2023 게임설명 바로가기"><img src="./img/hover_gamelist10.png" alt="마구마구 2023 로고"></a>
+        <a href="#" target="_blank" title="마구마구 2023 공식사이트 바로가기">공식사이트<i class="fas fa-angle-right"></i></a>
+        <a href="#" target="_blank" title="마구마구 2023 구글플레이 바로가기"><img src="./img/btn_playstore.png" alt="구글플레이 바로가기"></a>
+        <a href="#" target="_blank" title="마구마구 2023 앱스토어 바로가기"><img src="./img/btn_appstore.png" alt="앱스토어 바로가기"></a>
+      </div>
+    </li>
+
+    <li>
+      <div class="gamelist-gameinfo">
+        <div class="gamelist-img img11"></div>
+        <figure>
+          <img src="./img/gamelist11.png" alt="킹오브파이터 올스타 로고">
+          <figcaption>
+            <p class="gamelist-title">킹오브파이터 올스타</p>
+            <p class="gamelist-type">RPG</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="gamelist-gameinfo-hover">
+        <a href="#" target="_blank" title="킹오브파이터 올스타 게임설명 바로가기"><img src="./img/hover_gamelist11.png" alt="킹오브파이터 올스타 로고"></a>
+        <a href="#" target="_blank" title="킹오브파이터 올스타 공식사이트 바로가기">공식사이트<i class="fas fa-angle-right"></i></a>
+        <a href="#" target="_blank" title="킹오브파이터 올스타 구글플레이 바로가기"><img src="./img/btn_playstore.png" alt="구글플레이 바로가기"></a>
+        <a href="#" target="_blank" title="킹오브파이터 올스타 앱스토어 바로가기"><img src="./img/btn_appstore.png" alt="앱스토어 바로가기"></a>
+      </div>
+    </li>
+
+    <li>
+      <div class="gamelist-gameinfo">
+        <div class="gamelist-img img12"></div>
+        <figure>
+          <img src="./img/gamelist12.png" alt="BTS Universe Story 로고">
+          <figcaption>
+            <p class="gamelist-title">BTS Universe Story</p>
+            <p class="gamelist-type">캐주얼</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="gamelist-gameinfo-hover">
+        <a href="#" target="_blank" title="BTS Universe Story 게임설명 바로가기"><img src="./img/hover_gamelist12.png" alt="BTS Universe Story 로고"></a>
+        <a href="#" target="_blank" title="BTS Universe Story 공식사이트 바로가기">공식사이트<i class="fas fa-angle-right"></i></a>
+        <a href="#" target="_blank" title="BTS Universe Story 구글플레이 바로가기"><img src="./img/btn_playstore.png" alt="구글플레이 바로가기"></a>
+        <a href="#" target="_blank" title="BTS Universe Story 앱스토어 바로가기"><img src="./img/btn_appstore.png" alt="앱스토어 바로가기"></a>
+      </div>
+    </li>
+
+  </ul>
+  `;
+
 });
--- game/script/m_game.js
+++ game/script/m_game.js
@@ -60,7 +60,7 @@ $(function(){
   ////////////////// 게임 리스트 (탭메뉴) //////////////////////
 
   // 1. 탭메뉴 전환 스크립트
-  $('.gamelist-tab > li').click(function(){
+  $('.gamelist-tab > li').click(function () {
     $(this).siblings().removeClass('gamelist-tab-on');
     $(this).addClass('gamelist-tab-on');
 
@@ -68,50 +68,113 @@ $(function(){
     $('.gamelist-list').eq(gamelist_index).addClass('on').siblings().removeClass('on')
   });
 
+  // 2. PC에서만 더보기 버튼 나오게 하기
+  // 단, 다시 PC 눌렀을때는 더보기 버튼이 나오게 하기
+  $('.gamelist-tab > li:first-child').click(function () {
+    $('.game-btn').show();
+    $('.gamelist-list-add').hide();
+  });
 
-  // // 2. json 게임리스트 삽입 스크립트
-  // $('.game-btn').click(function () {
-  //   $(this).hide(); // 더보기버튼 숨기기
+  $('.gamelist-tab > li:nth-child(2)').click(function () {
+    $('.gamelist-list-add').hide();
+    $('.game-btn').hide();
+  });
 
-  //   $.ajax({
-  //     url: './game.json',
-  //     type: 'post',
-  //     dataType: 'json',
-  //     success: function (result) {
-  //       let t = '<ul class="gamelist-list">';
-  //       $.each(result.game, function(i,e) {
-  //         t += `
-  //           <li>
-  //             <div class="gamelist-gameinfo">
-  //               <div class="gamelist-img ${e.img_class}"></div>
-  //               <figure>
-  //                 <img src="./img/${e.icon_path}" alt="${e.title} 로고">
-  //                 <figcaption>
-  //                   <p class="gamelist-title">${e.title}</p>
-  //                   <p class="gamelist-type">${e.type}</p>
-  //                 </figcaption>
-  //               </figure>
-  //             </div>
-  //             <div class="gamelist-gameinfo-hover display-none">
-  //               <a href="#" target="_blank" title="${e.title} 게임설명 바로가기"><img src="./img/${e.logo_path}" alt="${e.title} 로고"></a>
-  //               <a href="#" target="_blank" title="${e.title} 공식사이트 바로가기">공식사이트<i class="fas fa-angle-right"></i></a>
-  //               <a href="#" target="_blank" title="${e.title} 구글플레이 바로가기"><img src="./img/btn_playstore.png" alt="구글플레이 바로가기"></a>
-  //               <a href="#" target="_blank" title="${e.title} 앱스토어 바로가기"><img src="./img/btn_appstore.png" alt="앱스토어 바로가기"></a>
-  //             </div>
-  //             <div class="gamelist-margin"></div>
-  //           </li>
-  //         `;
-  //       });
-  //       t += "</ul>";
-  //       //데이터를 t변수에 담아서 list박스에 내용을 출력한다.
-  //       $('.gamelist-list-add').html(t);
-  //     }
-  //   });
-  //   return false;
-  // });
+  $('.gamelist-tab > li:last-child').click(function () {
+    $('.gamelist-list-add').hide();
+    $('.game-btn').hide();
+  });
 
 
+  // 3. 더보기 버튼 클릭시 추가
+  $('.game-btn').click(function(){
+    $('.gamelist-list-add').html(content);
+    $('.gamelist-list-add').show();
+    $(this).hide();
+  });
 
 
+  let content =
+  `
+  <ul class="gamelist-list2">
+    <li>
+      <div class="gamelist-gameinfo">
+        <div class="gamelist-img img09"></div>
+        <figure>
+          <img src="./img/gamelist09.png" alt="쿵야 캐치마인드 로고">
+          <figcaption>
+            <p class="gamelist-title">쿵야 캐치마인드</p>
+            <p class="gamelist-type">캐주얼</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="gamelist-gameinfo-hover">
+        <a href="#" target="_blank" title="쿵야 캐치마인드 게임설명 바로가기"><img src="./img/hover_gamelist09.png" alt="쿵야 캐치마인드 로고"></a>
+        <a href="#" target="_blank" title="쿵야 캐치마인드 공식사이트 바로가기">공식사이트<i class="fas fa-angle-right"></i></a>
+        <a href="#" target="_blank" title="쿵야 캐치마인드 구글플레이 바로가기"><img src="./img/btn_playstore.png" alt="구글플레이 바로가기"></a>
+        <a href="#" target="_blank" title="쿵야 캐치마인드 앱스토어 바로가기"><img src="./img/btn_appstore.png" alt="앱스토어 바로가기"></a>
+      </div>
+    </li>
 
+    <li>
+      <div class="gamelist-gameinfo">
+        <div class="gamelist-img img10"></div>
+        <figure>
+          <img src="./img/gamelist10.png" alt="마구마구 2023 로고">
+          <figcaption>
+            <p class="gamelist-title">마구마구 2023</p>
+            <p class="gamelist-type">스포츠</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="gamelist-gameinfo-hover">
+        <a href="#" target="_blank" title="마구마구 2023 게임설명 바로가기"><img src="./img/hover_gamelist10.png" alt="마구마구 2023 로고"></a>
+        <a href="#" target="_blank" title="마구마구 2023 공식사이트 바로가기">공식사이트<i class="fas fa-angle-right"></i></a>
+        <a href="#" target="_blank" title="마구마구 2023 구글플레이 바로가기"><img src="./img/btn_playstore.png" alt="구글플레이 바로가기"></a>
+        <a href="#" target="_blank" title="마구마구 2023 앱스토어 바로가기"><img src="./img/btn_appstore.png" alt="앱스토어 바로가기"></a>
+      </div>
+    </li>
+
+    <li>
+      <div class="gamelist-gameinfo">
+        <div class="gamelist-img img11"></div>
+        <figure>
+          <img src="./img/gamelist11.png" alt="킹오브파이터 올스타 로고">
+          <figcaption>
+            <p class="gamelist-title">킹오브파이터 올스타</p>
+            <p class="gamelist-type">RPG</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="gamelist-gameinfo-hover">
+        <a href="#" target="_blank" title="킹오브파이터 올스타 게임설명 바로가기"><img src="./img/hover_gamelist11.png" alt="킹오브파이터 올스타 로고"></a>
+        <a href="#" target="_blank" title="킹오브파이터 올스타 공식사이트 바로가기">공식사이트<i class="fas fa-angle-right"></i></a>
+        <a href="#" target="_blank" title="킹오브파이터 올스타 구글플레이 바로가기"><img src="./img/btn_playstore.png" alt="구글플레이 바로가기"></a>
+        <a href="#" target="_blank" title="킹오브파이터 올스타 앱스토어 바로가기"><img src="./img/btn_appstore.png" alt="앱스토어 바로가기"></a>
+      </div>
+    </li>
+
+    <li>
+      <div class="gamelist-gameinfo">
+        <div class="gamelist-img img12"></div>
+        <figure>
+          <img src="./img/gamelist12.png" alt="BTS Universe Story 로고">
+          <figcaption>
+            <p class="gamelist-title">BTS Universe Story</p>
+            <p class="gamelist-type">캐주얼</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="gamelist-gameinfo-hover">
+        <a href="#" target="_blank" title="BTS Universe Story 게임설명 바로가기"><img src="./img/hover_gamelist12.png" alt="BTS Universe Story 로고"></a>
+        <a href="#" target="_blank" title="BTS Universe Story 공식사이트 바로가기">공식사이트<i class="fas fa-angle-right"></i></a>
+        <a href="#" target="_blank" title="BTS Universe Story 구글플레이 바로가기"><img src="./img/btn_playstore.png" alt="구글플레이 바로가기"></a>
+        <a href="#" target="_blank" title="BTS Universe Story 앱스토어 바로가기"><img src="./img/btn_appstore.png" alt="앱스토어 바로가기"></a>
+      </div>
+    </li>
+
+  </ul>
+  `;
+
+  
 });
