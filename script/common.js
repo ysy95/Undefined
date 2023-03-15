e3726bb0fd72fee451cb25b65270c1133c5814d2
ysy95 <ysy95@gmail.com>
2023-03-15 14:49:09 +0900

    로고이미지 및 패밀리사이트

--- script/common.js
+++ script/common.js
@@ -34,3 +34,14 @@ $.ajax({
 
 });
 
+
+
+
+function siteUrl(s){//사용자가 선택목록을 선택하면 siteUrl함수가 호출됨.
+  if(s.value!='none'){//옵션을 선택하면
+    window.open(s.value,'_self');//선택한 사이트가 열리게 하고 
+    s.value='none';//기존값을지운다
+  }else{//선택하지 않으면
+    return s;//프로그램을 종료
+  }
+}
